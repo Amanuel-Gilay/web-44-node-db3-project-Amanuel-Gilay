c1ca9c30a414433263a26f46901e2c6116bdaeb0
Amanuel-Gilay <emahawi27@gmail.com>
2021-08-11 22:43:17 -0700

    wip

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,3 +1,4 @@
+//const db = require('../../data/db-config') 
 function find() { // EXERCISE A
   /*
     1A- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`.
@@ -15,6 +16,11 @@ function find() { // EXERCISE A
     2A- When you have a grasp on the query go ahead and build it in Knex.
     Return from this function the resulting dataset.
   */
+ return db('schemes as sc')
+ .leftjoin(`steps as st`, `sc.scheme_id`)
+ .select(`sc*`)
+ .count(`st.step_id as number_of_steps`)
+ .groupBy(`sc.scheme_id`)
 }
 
 function findById(scheme_id) { // EXERCISE B
@@ -108,7 +114,7 @@ function findSteps(scheme_id) { // EXERCISE C
   */
 }
 
-function add(scheme) { // EXERCISE D
+function db(scheme) { // EXERCISE D
   /*
     1D- This function creates a new scheme and resolves to _the newly created scheme_.
   */
@@ -126,6 +132,7 @@ module.exports = {
   find,
   findById,
   findSteps,
-  add,
   addStep,
+  db,
+
 }
